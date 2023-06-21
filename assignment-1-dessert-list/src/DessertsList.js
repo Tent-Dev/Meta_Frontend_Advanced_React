6a93fb8607ad884f3f30af96d62948db2de4b176
Chutipas Borsub <45554395+Tent-Dev@users.noreply.github.com>
2023-06-21 15:42:00 +0700

    Update DessertsList.js

--- assignment-1-dessert-list/src/DessertsList.js
+++ assignment-1-dessert-list/src/DessertsList.js
@@ -2,11 +2,11 @@ function DessertsList(props) {
   // Implement the component here.
   return (
     <ul>
-      {props.data.sort((a, b) => a.calories - b.calories).map((e) => {
-        if (e.calories < 500) {
-          const showData = e.name + ' - ' + e.calories;
-          return <li>{showData}</li>
-        }
+      {props.data.sort((a, b) => a.calories - b.calories).filter((e) => e.calories < 500).map((e) => {
+
+        const showData = e.name + ' - ' + e.calories;
+        return <li>{showData}</li>
+
       })}
     </ul>
   );
